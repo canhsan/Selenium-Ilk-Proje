971418ed7f2b560ee51e573cfebb9cb6ccb3ffed
canhsan <1979hasancan@gmail.com>
2023-05-03 11:22:21 +0300

    4.3/05/2023

--- src/test/java/day02_driverMethodlari/C02_ilkTest.java
+++ src/test/java/day02_driverMethodlari/C02_ilkTest.java
@@ -1,7 +1,10 @@
 package day02_driverMethodlari;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class C02_ilkTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
         1. Yeni bir package olusturalim : day01
         2. Yeni bir class olusturalim : C03_GetMethods
@@ -14,7 +17,39 @@ public class C02_ilkTest {
         9. Sayfa HTML kodlarinda “alisveris” kelimesi gectigini test edin
         10. Sayfayi kapatin.
          */
+        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver_win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
 
+        System.out.println("Sayfa Başlığı :" + driver.getTitle());
+        String expectedIcerik = "Amazon";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.contains(expectedIcerik)) {
+            System.out.println("Title amazon içermektedir, test PASSED");
+        } else {
+            System.out.println("Title amazon içermemektedir, test FAİLED");
+        }
+        expectedIcerik = "amazon";
+        String actualUrl= driver.getCurrentUrl();
+
+         if (actualUrl.contains(expectedIcerik)){
+             System.out.println(driver.getCurrentUrl()+"URL amazon içeriyor, URL Test PASSED");
+         }else{
+             System.out.println("URL amazon içermiyor, URL Test FAİLED");
+         }
+        System.out.println("Sayfanın handle değeri: "+ driver.getWindowHandle());
+         expectedIcerik="alısveris";
+         String actualPageSource=driver.getPageSource();
+         if (actualPageSource.contains(expectedIcerik)){
+             System.out.println("Sayfa kaynak kodları alısveris içeriyor,test PASSED");
+         }else{
+             System.out.println("Sayfa kaynak kodları alısveris içermiyor,test FAİLED");
+
+         }
+
+        Thread.sleep(3000);
+        driver.close();
 
 
     }
