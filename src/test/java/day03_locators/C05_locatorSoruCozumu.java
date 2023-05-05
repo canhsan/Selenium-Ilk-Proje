569ebbca45c6db3dff27ca6a4f6fa1762b98ae14
canhsan <1979hasancan@gmail.com>
2023-05-05 14:19:29 +0300

    1.5/05/2023

--- src/test/java/day03_locators/C05_locatorSoruCozumu.java
+++ src/test/java/day03_locators/C05_locatorSoruCozumu.java
@@ -7,26 +7,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C03_webElementMethodları {
+public class C05_locatorSoruCozumu {
     public static void main(String[] args) throws InterruptedException {
+        /*
+        1- https://www.amazon.com/ sayfasına gidin.
+        2- Arama kutusuna “city bike” yazip aratin
+        3- Görüntülenen sonuçların sayısını yazdırın
+        4- Listeden ilk urunun resmine tıklayın.
+        */
+
         System.setProperty("webdriver.chrome.driver","src/resources/chromedriver_win32/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
+
+        //1- https://www.amazon.com/ sayfasına gidin.
+
         driver.get("https://www.amazon.com");
-        WebElement aramaKutusuElementi= driver.findElement(By.id("twotabsearchtextbox"));
 
-        System.out.println(aramaKutusuElementi.isDisplayed());
-        System.out.println(aramaKutusuElementi.isEnabled());
-        aramaKutusuElementi.isSelected();
-        System.out.println(aramaKutusuElementi.getText());
-        aramaKutusuElementi.sendKeys("hasan can");
-        Thread.sleep(2000);
-        aramaKutusuElementi.clear();
+        //2- Arama kutusuna “city bike” yazip aratin
+
+        WebElement aramaKutusuElementi=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusuElementi.sendKeys("city bike");
+        aramaKutusuElementi.submit();
 
 
-        Thread.sleep(1000);
+
+
+
+
+
+
+        Thread.sleep(3000);
         driver.close();
 
     }
