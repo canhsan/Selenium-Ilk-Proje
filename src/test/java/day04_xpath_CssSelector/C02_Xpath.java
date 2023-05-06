f6755f2d42c8ba45dd1abfc1c62bc4e77bf5de62
canhsan <1979hasancan@gmail.com>
2023-05-06 13:19:22 +0300

    1.6/05/2023

---
+++ src/test/java/day04_xpath_CssSelector/C02_Xpath.java
@@ -0,0 +1,46 @@
+package day04_xpath_CssSelector;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_Xpath {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver_win32/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+        //1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+        // 2- Add Element butonuna basin
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/button")).click();
+        Thread.sleep(3000);
+
+       // 3- Delete butonu’nun gorunur oldugunu test edin
+        WebElement deleteButtonElementi= driver.findElement(By.xpath("//*[@id=\"elements\"]/button"));
+        if (deleteButtonElementi.isDisplayed()){
+            System.out.println("Delete button testi is PASSED");
+        }else{
+            System.out.println("Delete button testi is FAİLED");
+        }
+       // 4- Delete tusuna basin
+        deleteButtonElementi.click();
+       // 5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
+        WebElement addElementsElementi=driver.findElement(By.xpath("//*[@id=\"content\"]/h3"));
+
+        if (addElementsElementi.isDisplayed()){
+            System.out.println("Add/RemoveElementi testi is PASSED");
+        }else{
+            System.out.println("Add/RemoveElementi i is FAİLED");
+        }
+
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
